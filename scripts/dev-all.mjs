7593c37b4083f6085e3b6cb11f0440ade53bcf6f
lmadisha <legae.madisha@digitaltwin.digital>
2026-05-11 15:34:57 +0200

    Reverted the frontend host, back to original 192.168.0.184

--- scripts/dev-all.mjs
+++ scripts/dev-all.mjs
@@ -72,7 +72,7 @@ startProcess(
     path.join(rootDir, "node_modules", "nuxt", "bin", "nuxt.mjs"),
     "dev",
     "--host",
-    "192.168.1.144",
+    "192.168.0.184",
     "--port",
     "5174",
   ],
